8bfc5088f30f3169c0dfc55b375e733b69faa6e4
Sai Sudheer <saisudheerkancharlapalli18@gmail.com>
2025-11-21 15:21:01 +0530

    ATM-PROJECT

---
+++ src/atm/AtmInterFace.java
@@ -0,0 +1,10 @@
+package atm;
+
+public interface AtmInterFace {
+	public void viewBalance();
+	public void withdrawAmount(double withdrawAmount);
+	public void depositAmount(double depositAmount);
+	public void viewMiniStatement();
+	
+
+}
